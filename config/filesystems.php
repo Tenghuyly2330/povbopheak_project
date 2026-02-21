1f4d46e077b86b88335536cf1185cd347e1c5416
Tenghuy <lytenghuy8@gmail.com>
2026-02-21 21:26:44 +0700

    update latest

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL'), '/').'/storage',
+            'root' => storage_path('../public/storage'),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
